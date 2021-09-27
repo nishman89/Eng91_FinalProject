583db4cf993ab617c8b11452c20144654e060a78
michael carini <snownkc@icloud.com>
2021-09-27 17:11:41 +0100

    did the pom setup

--- Eng91FinalProject/Eng91FinalProject/lib/pages/BulkImport/BulkImportProfilesPage.cs
+++ Eng91FinalProject/Eng91FinalProject/lib/pages/BulkImport/BulkImportProfilesPage.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
 
 namespace Eng91FinalProject.lib.pages
 {
-    class BulkImportProfilesPage
+    public class BulkImportProfilesPage
     {
+        #region Properties
+
+        private IWebDriver _seleniumDriver;
+        private IWebElement _findChooseFileButton => _seleniumDriver.FindElement(By.Id("File"));
+        private IWebElement _findSaveButton => _seleniumDriver.FindElement(By.Id("saveButton"));
+
+        #endregion
+
+
+
+        public void ClickCreateNewTracker() => _findChooseFileButton.Click();
+        public void ClickEditButton() => _findSaveButton.Click();
     }
 }
--- Eng91FinalProject/Eng91FinalProject/lib/pages/TraineeTrackerPage.cs
+++ Eng91FinalProject/Eng91FinalProject/lib/pages/TraineeTrackerPage.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
 
 namespace Eng91FinalProject.lib.pages
 {
-    class TraineeTrackerPage
+    public class TraineeTrackerPage
     {
+        #region Properties
+
+        private IWebDriver _seleniumDriver;
+        private IWebElement _findCreateNewTracker => _seleniumDriver.FindElement(By.Id("CreateNewTrackerButton"));
+        private IWebElement _findEditButton => _seleniumDriver.FindElement(By.Id("EditTracker"));
+
+        #endregion
+
+
+
+        public void ClickCreateNewTracker() => _findCreateNewTracker.Click();
+        public void ClickEditButton() => _findEditButton.Click();
+
+
+
+
     }
 }

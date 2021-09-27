c556d9f7a4fbfc019e6517cd98afd941e30db8d7
Alex Rae <arae@spartaglobal.com>
2021-09-27 16:33:42 +0100

    Added Skeleton

---
+++ Eng91FinalProject/Eng91FinalProject/AppConfigReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Configuration;
+
+namespace Eng91FinalProject
+{
+    public static class AppConfigReader
+    {
+        public static readonly string BaseURL = ConfigurationManager.AppSettings["base_url"];
+    }
+}

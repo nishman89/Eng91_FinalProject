0b75a59ca09e09216c6f87627266400664a16e22
Gareth <GMitchell@Spartaglobal.com>
2021-09-27 20:12:17 +0100

    Added Trainee Service

--- Eng91FinalProject/Eng91FinalProjectAPI/HTTPManager/CallManager.cs
+++ Eng91FinalProject/Eng91FinalProjectAPI/HTTPManager/CallManager.cs
@@ -9,35 +9,26 @@ namespace Eng91FinalProjectAPI.HTTPManager
         private readonly IRestClient _client;
 
         public int StatusCode { get; set; }
+        public enum GetMethods
+        {
+            Trainees,
+            Profiles
+        }
 
         public CallManager()
         {
             _client = new RestClient(AppConfigReader.BaseUrl);
         }
 
-        public CallManager(string uri)
-        {
-            _client = new RestClient(uri);
-        }
-
-        public async Task<string> MakeTraineeRequestAsync()
+        public async Task<string> MakeGetRequestAsync(GetMethods method)
         {
             var request = new RestRequest();
             request.AddHeader("Content-Type", "application/json");
-            request.Resource = $"/trainees";
+            request.Resource = $"/{method.ToString().ToLower()}";
             IRestResponse response = await _client.ExecuteAsync(request);
             StatusCode = (int)response.StatusCode;
             return response.Content;
         }
 
-        public async Task<string> MakeProfileRequestAsync()
-        {
-            var request = new RestRequest();
-            request.AddHeader("Content-Type", "application/json");
-            request.Resource = $"/profiles";
-            IRestResponse response = await _client.ExecuteAsync(request);
-            StatusCode = (int)response.StatusCode;
-            return response.Content;
-        }
     }
 }

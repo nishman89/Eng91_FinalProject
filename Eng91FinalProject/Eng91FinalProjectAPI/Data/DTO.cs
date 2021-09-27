102050513a868b53298aeb8009c6bc301c144763
Alex Rae <arae@spartaglobal.com>
2021-09-27 15:57:56 +0100

    Initialised Document

---
+++ Eng91FinalProject/Eng91FinalProjectAPI/Data/DTO.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Eng91FinalProjectAPI.Data
+{
+    public class DTO<ResponseType> where ResponseType : IResponse, new()
+    {
+        public ResponseType Response { get; set; }
+    }
+    public void Deserialize(string response)
+    {
+        Response = JsonConvert.DeserializeObject<ResponseType>(response);
+    }
+}

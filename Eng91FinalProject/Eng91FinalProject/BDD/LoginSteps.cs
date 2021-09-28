189591cae9e2801d5bb0ce6cc51e04b10787e197
Alex Rae <arae@spartaglobal.com>
2021-09-28 17:13:14 +0100

    fixed stuff

---
+++ Eng91FinalProject/Eng91FinalProject/BDD/LoginSteps.cs
@@ -0,0 +1,17 @@
+﻿using Eng91FinalProject.lib.pages;
+using Eng91FinalProject.utils;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using NUnit.Framework;
+
+namespace Eng91FinalProject
+{
+    [Binding]
+    [Scope(Feature = "Login")]
+    public class LoginSteps : SharedLoginSteps
+    {
+         
+    }
+}

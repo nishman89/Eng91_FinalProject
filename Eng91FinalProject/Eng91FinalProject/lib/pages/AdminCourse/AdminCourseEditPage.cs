2bfa7d14d3ea2f4e8a1c890fd3f701e7a1e3b31c
Benjamin <h8bmf@hotmail.co.uk>
2021-09-28 09:55:13 +0100

    Fixed some mistakes in the pages

--- Eng91FinalProject/Eng91FinalProject/lib/pages/AdminCourse/AdminCourseEditPage.cs
+++ Eng91FinalProject/Eng91FinalProject/lib/pages/AdminCourse/AdminCourseEditPage.cs
@@ -14,7 +14,7 @@ namespace Eng91FinalProject.lib.pages
         private IWebElement _trainerName => _seleniumDriver.FindElement(By.Id("TrainerId"));
         private IWebElement _streamName => _seleniumDriver.FindElement(By.Id("StreamId"));
         private IWebElement _locationName => _seleniumDriver.FindElement(By.Id("Location"));
-        private IWebElement _saveButton => _seleniumDriver.FindElement(By.CssSelector("[clas='btn btn-primary m-2']"));
+        private IWebElement _saveButton => _seleniumDriver.FindElement(By.CssSelector("[class='btn btn-primary m-2']"));
         public AdminCourseEditPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
         public void SaveButton() => _saveButton.Click();
     }

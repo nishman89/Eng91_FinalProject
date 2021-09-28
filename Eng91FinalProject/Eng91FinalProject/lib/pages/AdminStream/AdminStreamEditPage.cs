2c1985d5415453046e8fc0dbb9d988fea3cdb83a
michael carini <snownkc@icloud.com>
2021-09-28 11:55:31 +0100

    finished Admin Stream POM

--- Eng91FinalProject/Eng91FinalProject/lib/pages/AdminStream/AdminStreamEditPage.cs
+++ Eng91FinalProject/Eng91FinalProject/lib/pages/AdminStream/AdminStreamEditPage.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace Eng91FinalProject.lib.pages
 {
-    class AdminStreamEditPage
+    public class AdminStreamEditPage
     {
+        #region Properties
+
+        private IWebDriver _seleniumDriver;
+        private IWebElement _findEditBar => _seleniumDriver.FindElement(By.Id("Name"));
+        private IWebElement _findSaveBtnn => _seleniumDriver.FindElement(By.CssSelector("[class='btn btn-primary m-2']"));
+
+        #endregion
+
+        public void ClickDeleteBar(string data) => _findEditBar.SendKeys(data);
+        public void ClickBackToList() => _findSaveBtnn.Click();
     }
 }

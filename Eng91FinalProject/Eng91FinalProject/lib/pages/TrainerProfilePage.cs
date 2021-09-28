30ca6cbd5bb34d736e86001610aaa749997e4806
swase <fg2g15@soton.ac.uk>
2021-09-28 11:16:10 +0100

    Trainer Profile POM

--- Eng91FinalProject/Eng91FinalProject/lib/pages/TrainerProfilePage.cs
+++ Eng91FinalProject/Eng91FinalProject/lib/pages/TrainerProfilePage.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+
 
 namespace Eng91FinalProject.lib.pages
 {
     public class TrainerProfilePage
     {
+        private IWebDriver _seleniumDriver { get; set; }
+
+        //Methods
+        public TrainerProfilePage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+        private IWebElement _editButton => _seleniumDriver.FindElement(By.Id("EditButton"));
+        private IWebElement _email => _seleniumDriver.FindElement(By.Id("EditButton"));
+        private IWebElement _location => _seleniumDriver.FindElement(By.Id("EditButton"));
+        private IWebElement _streams => _seleniumDriver.FindElement(By.Id("TrainerStreams"));
+        private IWebElement _profileName => _seleniumDriver.FindElement(By.ClassName("mt-3"));
 
     }
 }

d681d0989b52868440f63736a77dafa177bcfe78
Benjamin <h8bmf@hotmail.co.uk>
2021-09-28 12:06:17 +0100

    Css corrections made to pages

--- Eng91FinalProject/Eng91FinalProject/lib/pages/AdminCourse/AdminCoursesPage.cs
+++ Eng91FinalProject/Eng91FinalProject/lib/pages/AdminCourse/AdminCoursesPage.cs
@@ -10,15 +10,13 @@ namespace Eng91FinalProject.lib.pages
     public class AdminCoursesPage
     {
         private IWebDriver _seleniumDriver;
-        private IWebElement _createNewCourse => _seleniumDriver.FindElement(By.CssSelector("[class='btn btn - primary col - sm - 12 mb - 4']"));
-        private IWebElement _editCourse => _seleniumDriver.FindElement(By.CssSelector("[class='fas fa-edit pr-2']"));
-        private IWebElement _deleteCourse => _seleniumDriver.FindElement(By.CssSelector("[class='float-right btn btn-danger ml-2']"));
-        private IWebElement _searchButton => _seleniumDriver.FindElement(By.CssSelector("[class='btn btn - primary']"));
+        private IWebElement _createNewCourse => _seleniumDriver.FindElement(By.CssSelector(".col-sm-12"));
+        private IWebElement _searchButton => _seleniumDriver.FindElement(By.CssSelector(".form-group.btn"));
         private IWebElement _searchBar => _seleniumDriver.FindElement(By.Name("searchString"));
         public AdminCoursesPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
         public void CreateNewCourse() => _createNewCourse.Click();
-        public void EditCourse() => _editCourse.Click();
-        public void DeleteCourse() => _deleteCourse.Click();
+        public void EditCourse(string courseNum) => _seleniumDriver.FindElement(By.CssSelector($"#Engineering-{courseNum}.btn-primary")).Click();
+        public void DeleteCourse(string courseNum) => _seleniumDriver.FindElement(By.CssSelector($"#Engineering-{courseNum}.btn-danger")).Click();
         public void SearchCourse() => _searchButton.Click();
         public void Search(string course) => _searchBar.SendKeys(course);
     }

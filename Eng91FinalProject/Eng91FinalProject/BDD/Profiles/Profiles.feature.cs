95b6c8e2ac7c6faf527e27f7acfcfe090bd4d625
swase <fg2g15@soton.ac.uk>
2021-09-30 13:48:29 +0100

    Added Profiles POM, features and steps

--- Eng91FinalProject/Eng91FinalProject/BDD/Profiles/Profiles.feature.cs
+++ Eng91FinalProject/Eng91FinalProject/BDD/Profiles/Profiles.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Eng91FinalProject.BDD
+namespace Eng91FinalProject.BDD.Profiles
 {
     using TechTalk.SpecFlow;
     using System;
@@ -20,22 +20,22 @@ namespace Eng91FinalProject.BDD
     [System.CodeDom.Compiler.GeneratedCodeAttribute("TechTalk.SpecFlow", "3.9.0.0")]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [NUnit.Framework.TestFixtureAttribute()]
-    [NUnit.Framework.DescriptionAttribute("TrainerHomePage")]
-    public partial class TrainerHomePageFeature
+    [NUnit.Framework.DescriptionAttribute("Profiles")]
+    public partial class ProfilesFeature
     {
         
         private TechTalk.SpecFlow.ITestRunner testRunner;
         
         private string[] _featureTags = ((string[])(null));
         
-#line 1 "TrainerHomePage.feature"
+#line 1 "Profiles.feature"
 #line hidden
         
         [NUnit.Framework.OneTimeSetUpAttribute()]
         public virtual void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "BDD", "TrainerHomePage", "\tSimple tests conducted within the Trainer Home Page", ProgrammingLanguage.CSharp, ((string[])(null)));
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "BDD/Profiles", "Profiles", "\tSimple calculator for adding two numbers", ProgrammingLanguage.CSharp, ((string[])(null)));
             testRunner.OnFeatureStart(featureInfo);
         }
         
@@ -74,15 +74,15 @@ namespace Eng91FinalProject.BDD
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("As Trainer, view all test results")]
-        [NUnit.Framework.CategoryAttribute("TrainerTestResults")]
-        public virtual void AsTrainerViewAllTestResults()
+        [NUnit.Framework.DescriptionAttribute("View student profile 1")]
+        [NUnit.Framework.CategoryAttribute("Trainer_profiles_page")]
+        public virtual void ViewStudentProfile1()
         {
             string[] tagsOfScenario = new string[] {
-                    "TrainerTestResults"};
+                    "Trainer_profiles_page"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("As Trainer, view all test results", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 5
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("View student profile 1", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 6
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -102,35 +102,38 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-                TechTalk.SpecFlow.Table table39 = new TechTalk.SpecFlow.Table(new string[] {
-                            "email",
-                            "password"});
-                table39.AddRow(new string[] {
+                TechTalk.SpecFlow.Table table15 = new TechTalk.SpecFlow.Table(new string[] {
+                            "Email",
+                            "Password"});
+                table15.AddRow(new string[] {
                             "jsmith@spartaglobal.com",
                             "Password123!"});
-#line 6
- testRunner.Given("I am logged in", ((string)(null)), table39, "Given ");
+#line 7
+testRunner.Given("I am logged in", ((string)(null)), table15, "Given ");
 #line hidden
-#line 9
- testRunner.When("I click EXPAND ALL", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 10
+testRunner.When("I click \'Profiles\'", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
 #line 11
- testRunner.Then("all test results are shown", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+testRunner.And("I click on a trainee\'s profile", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 12
+testRunner.Then("I can view that trainee\'s profile", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("As Trainer, view individual test results")]
-        [NUnit.Framework.CategoryAttribute("TrainerTestResults")]
-        public virtual void AsTrainerViewIndividualTestResults()
+        [NUnit.Framework.DescriptionAttribute("View all Student Profiles")]
+        [NUnit.Framework.CategoryAttribute("Trainer_profiles_page")]
+        public virtual void ViewAllStudentProfiles()
         {
             string[] tagsOfScenario = new string[] {
-                    "TrainerTestResults"};
+                    "Trainer_profiles_page"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("As Trainer, view individual test results", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 14
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("View all Student Profiles", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 15
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -150,68 +153,20 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-                TechTalk.SpecFlow.Table table40 = new TechTalk.SpecFlow.Table(new string[] {
-                            "email",
-                            "password"});
-                table40.AddRow(new string[] {
+                TechTalk.SpecFlow.Table table16 = new TechTalk.SpecFlow.Table(new string[] {
+                            "Email",
+                            "Password"});
+                table16.AddRow(new string[] {
                             "jsmith@spartaglobal.com",
                             "Password123!"});
-#line 15
- testRunner.Given("I am logged in", ((string)(null)), table40, "Given ");
+#line 16
+testRunner.Given("I am logged in", ((string)(null)), table16, "Given ");
 #line hidden
-#line 18
- testRunner.When("I click a trainee", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 19
+testRunner.When("I click \'Profiles\'", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
 #line 20
- testRunner.Then("That trainee\'s test results are shown", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line hidden
-            }
-            this.ScenarioCleanup();
-        }
-        
-        [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("As Trainer, view Progress Tracker")]
-        [NUnit.Framework.CategoryAttribute("ProgressTrackerNavigation")]
-        public virtual void AsTrainerViewProgressTracker()
-        {
-            string[] tagsOfScenario = new string[] {
-                    "ProgressTrackerNavigation"};
-            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("As Trainer, view Progress Tracker", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 23
-this.ScenarioInitialize(scenarioInfo);
-#line hidden
-            bool isScenarioIgnored = default(bool);
-            bool isFeatureIgnored = default(bool);
-            if ((tagsOfScenario != null))
-            {
-                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((this._featureTags != null))
-            {
-                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((isScenarioIgnored || isFeatureIgnored))
-            {
-                testRunner.SkipScenario();
-            }
-            else
-            {
-                this.ScenarioStart();
-                TechTalk.SpecFlow.Table table41 = new TechTalk.SpecFlow.Table(new string[] {
-                            "email",
-                            "password"});
-                table41.AddRow(new string[] {
-                            "jsmith@spartaglobal.com",
-                            "Password123!"});
-#line 24
- testRunner.Given("I am logged in", ((string)(null)), table41, "Given ");
-#line hidden
-#line 27
- testRunner.When("I click PROGRESS TRACKER", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line hidden
-#line 29
- testRunner.And("I am taken to the Progress Tracker page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+testRunner.Then("I can view all of the profiles", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
